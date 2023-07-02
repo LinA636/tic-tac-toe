6af5b800f3afbaefda2cb2c89a362cf52212eb4b
Lina <117147821+LinA636@users.noreply.github.com>
2023-07-02 14:24:37 +1200

    move files into lib/ directory

--- lib/board_game.rb
+++ lib/board_game.rb
@@ -6,10 +6,7 @@ class BoardGame
   attr_accessor :fields_arr, :current_player, :player1, :player2
 
   def initialize(player_one, player_two)
-    @fields_arr = []
-    9.times do
-      fields_arr.push(Field.new)
-    end
+    @fields_arr = Array.new(NUMBER_OF_FIELDS) {Field.new}
     @current_player = player_one
     @player1 = player_one
     @player2 = player_two
@@ -52,11 +49,7 @@ class BoardGame
   end
 
   def actualise_current_player
-    self.current_player = if current_player == player1
-                            player2
-                          else
-                            player1
-                          end
+    self.current_player = current_player == player1 ? player2 : player1
   end
 
   def announce_winner
@@ -72,27 +65,28 @@ class BoardGame
   private
 
   def check_3_same_symbols(three_fields)
-    (three_fields.any?(&:is_set) && (three_fields.map(&:symbol).uniq.length == 1))
+    three_fields.any?(&:is_set) && (three_fields.map(&:symbol).uniq.length == 1)
   end
 
   def check_player_field_number_input(number)
-    until player_input_is_number?(number)
-      print "#{current_player} choose a number between 1 and 9: "
-      number = gets.chomp.to_i
-    end
-    while check_field_occupied?(number)
-      print "#{current_player} choose another, unoccupied field: "
-      number = gets.chomp.to_i
-    end
-    number
-  end
-
-  def check_field_occupied?(field_number)
-    fields_arr[field_number - 1].is_set
+    number = player_input_is_number?(number)
+    check_field_occupied?(number)
   end
 
   def player_input_is_number?(field_number)
-    field_number.between?(1, 9)
+    until field_number.between?(1, 9)
+      print "#{current_player} choose a number between 1 and 9: "
+      field_number = gets.chomp.to_i
+    end
+    field_number
+  end
+
+  def check_field_occupied?(field_number)
+    while fields_arr[field_number - 1].is_set
+      print "#{current_player} choose another, unoccupied field: "
+      field_number = gets.chomp.to_i
+    end
+    field_number
   end
 
   def mark_field(number)
